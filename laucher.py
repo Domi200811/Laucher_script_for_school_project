ca09c0123b9a7ccbfcbefe3d21d4230b362b0866
Domi200811 <pekdomi2008@gmail.com>
2026-03-20 18:09:16 +0100

    Added .lower() and Internet outage proofing

--- laucher.py
+++ laucher.py
@@ -1,25 +1,31 @@
-
-
 import subprocess
 import sys
-    
+
+
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-APP_PATH="demo.py"  #Ide írd a fő program nevét/elérhetőségét
+
+APP_PATH = "demo.py"  # Ide írd a fő program nevét/elérhetőségét
 
 try:
     import blessed
 except ImportError:
-    print('A program futtatásához az alábbi könyvtár(ak) telepítése szükséges:')
-    print('    • blessed')
+    print("A program futtatásához az alábbi könyvtár(ak) telepítése szükséges:")
+    print("    • blessed")
     while True:
-        Answer=input('Szeretné telepíteni őket? ["yes"/"no"] ')
-        if Answer=="yes":
-            install("blessed")
-            subprocess.run([sys.executable, APP_PATH]) 
-            break
-        elif Answer=="no":
+        Answer = input('Szeretné telepíteni őket? ["yes"/"no"] ').lower()
+        if Answer == "yes":
+            try:
+                install("blessed")
+                subprocess.run([sys.executable, APP_PATH])
+                break
+            except:
+                print(
+                    "A varázsló hibába ütlözött a könyvtár telepítése során. (pedig nagyon keményen próbálkozott)"
+                )
+                break
+        elif Answer == "no":
             print("A program működése hibába ütközött")
             break
 else:
