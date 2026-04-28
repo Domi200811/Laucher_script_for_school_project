e735055cc11d5cab54be95a22d8b446949357966
Domi200811 <pekdomi2008@gmail.com>
2026-04-28 23:04:35 +0200

    Play all soundtrack files in a loop

--- demo.py
+++ demo.py
@@ -1,20 +1,23 @@
 import pygame
 import threading
+import os
 
 
 def music():
     pygame.mixer.init()
-    pygame.mixer.music.load("Action 52 - CheetahMen Theme.mp3")
-    pygame.mixer.music.play()
+    while True:
+        for music in os.listdir("soundtrack"):
+            pygame.mixer.music.load(f"soundtrack/{music}")
+            pygame.mixer.music.play()
 
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
 
 def text():
     print("lol")
     
 t1 = threading.Thread(target=music, daemon =True)
-t2 = threading.Thread(target=text, daemon=True)
+t2 = threading.Thread(target=text,)
 
 t1.start()
 t2.start()
