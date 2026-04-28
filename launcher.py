1738b5e428cd959e5f470608172827f449b52106
Domi200811 <pekdomi2008@gmail.com>
2026-04-28 18:10:07 +0200

    Added expandable laucher and implemented threading

--- launcher.py
+++ launcher.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import os
+import importlib
+import time
 
 
 def install(package):
@@ -11,28 +13,39 @@ def install(package):
 #                SZERKESZTHETŐ BEÁLLÍTÁSOK RÉSZE
 # =================================================================
 
-APP_PATH = "demo.py"     # Ide írd a fő program nevét/elérhetőségét
+APP_PATH = "demo.py"  # Ide írd a fő program nevét/elérhetőségét
 
 # =================================================================
 #                SZERKESZTHETŐ RÉSZ VÉGE
-# ================================================================= 
+# =================================================================
 
-APP_PATH=os.path.abspath(APP_PATH)
+APP_PATH = os.path.abspath(APP_PATH)
 
-try:
-    import blessed
-except ImportError:
+libs = ["blessed", "pygame"]
+missing_libs = []
+
+
+for lib in libs:
+    try:
+        importlib.import_module(lib)
+    except ImportError:
+        missing_libs.append(lib)
+
+
+if len(missing_libs) > 0:
     print("A program futtatásához az alábbi könyvtár(ak) telepítése szükséges:")
-    print("    • blessed")
+    for i in missing_libs:
+        print(f"• {i}")
     while True:
         answer = input('Szeretné telepíteni őket? ["yes"/"no"] ').lower()
         if answer == "yes" or answer == "y":
-                install("blessed")
-                break
+            for i in missing_libs:
+                install(i)
+            break
         elif answer == "no" or answer == "n":
             print("A program működése során hibába ütközött")
             sys.exit()
 
 print("Loading....")
+time.sleep(1)
 subprocess.run([sys.executable, APP_PATH])
-
