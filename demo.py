98de1a25d94a098eb01c7a843790f18f0aa26bb8
Domi200811 <pekdomi2008@gmail.com>
2026-05-18 22:43:58 +0200

    Bug fix
    Fixed a bug where the terminal would mess up at the rematch section if the user pressed a key on the keyboard

--- demo.py
+++ demo.py
@@ -228,6 +228,7 @@ def game(x_won, o_won):
                 Place = pygame.mixer.Sound(
                     f"sfx/place_sounds/{random.choice(os.listdir('sfx/place_sounds'))}"
                 )
+                
                 with term.location(y, x):
                     if following == 0:
                         print(term.red_bold + "ˇ" + term.normal)
@@ -256,149 +257,149 @@ def game(x_won, o_won):
                         if key == "a" or key.name == "KEY_LEFT":
                             y = y - 6
 
-                    if x > 9:
-                        x = x - 3
+                        if x > 9:
+                            x = x - 3
 
-                    if x < 0:
-                        x = x + 3
+                        if x < 0:
+                            x = x + 3
 
-                    if y > 18:
-                        y = y - 6
+                        if y > 18:
+                            y = y - 6
 
-                    if y < 3:
-                        y = y + 6
+                        if y < 3:
+                            y = y + 6
 
-                    if key == " " or key.name == "KEY_ENTER":
-                        if following == 0:
-                            sub_memory = [y, x + 1]
-                            if (
-                                sub_memory not in memory_X
-                                and sub_memory not in memory_O
-                            ):
-                                with term.location(y, x + 1):
-                                    Place.play()
-                                    print(term.white_bold + "X" + term.normal)
-                                    time.sleep(0.05)
-                                with term.location(y, x + 1):
-                                    print(term.yellow_bold + "X" + term.normal)
-                                    time.sleep(0.05)
-                                with term.location(y, x + 1):
-                                    print(term.red_bold + "X" + term.normal)
-                                    memory_X.append(sub_memory)
-                                    following = following + 1
-                                    if following > 1:
-                                        following = 0
-                        elif following == 1:
-                            sub_memory = [y, x + 1]
-                            if (
-                                sub_memory not in memory_X
-                                and sub_memory not in memory_O
-                            ):
-                                with term.location(y, x + 1):
-                                    Place.play()
-                                    print(term.white_bold + "O" + term.normal)
-                                    time.sleep(0.05)
-                                with term.location(y, x + 1):
-                                    print(term.yellow_bold + "O" + term.normal)
-                                    time.sleep(0.05)
-                                with term.location(y, x + 1):
-                                    print(term.blue_bold + "O" + term.normal)
-                                    memory_O.append(sub_memory)
-                                    following = following + 1
-                                    if following > 1:
-                                        following = 0
-                        for i in WINNING_COMBOS:
-                            if all(sub in memory_X for sub in i) == True:
+                        if key == " " or key.name == "KEY_ENTER":
+                            if following == 0:
+                                sub_memory = [y, x + 1]
+                                if (
+                                    sub_memory not in memory_X
+                                    and sub_memory not in memory_O
+                                ):
+                                    with term.location(y, x + 1):
+                                        Place.play()
+                                        print(term.white_bold + "X" + term.normal)
+                                        time.sleep(0.05)
+                                    with term.location(y, x + 1):
+                                        print(term.yellow_bold + "X" + term.normal)
+                                        time.sleep(0.05)
+                                    with term.location(y, x + 1):
+                                        print(term.red_bold + "X" + term.normal)
+                                        memory_X.append(sub_memory)
+                                        following = following + 1
+                                        if following > 1:
+                                            following = 0
+                            elif following == 1:
+                                sub_memory = [y, x + 1]
+                                if (
+                                    sub_memory not in memory_X
+                                    and sub_memory not in memory_O
+                                ):
+                                    with term.location(y, x + 1):
+                                        Place.play()
+                                        print(term.white_bold + "O" + term.normal)
+                                        time.sleep(0.05)
+                                    with term.location(y, x + 1):
+                                        print(term.yellow_bold + "O" + term.normal)
+                                        time.sleep(0.05)
+                                    with term.location(y, x + 1):
+                                        print(term.blue_bold + "O" + term.normal)
+                                        memory_O.append(sub_memory)
+                                        following = following + 1
+                                        if following > 1:
+                                            following = 0
+                            for i in WINNING_COMBOS:
+                                if all(sub in memory_X for sub in i) == True:
+                                    game_won = True
+                                    x_won[0] = x_won[0] + 1
+                                    current_winner = "X"
+                                    with open("ASCII/P1.txt", encoding="utf-8") as f:
+                                        for i in f:
+                                            if line_num == 0:
+                                                pygame.mixer.music.pause()
+                                                music_stop[0] = not music_stop[0]
+                                                P_slam.play()
+                                            with term.location(50, line_num):
+                                                print(i, end="")
+                                                line_num = line_num + 1
+                                                if line_num == 7:
+                                                    time.sleep(2)
+                                                    Win_slam.play()
+                                        time.sleep(3)
+                                        pygame.mixer.music.unpause()
+                                        music_stop[0] = not music_stop[0]
+                                if all(sub in memory_O for sub in i) == True:
+                                    game_won = True
+                                    o_won[0] = o_won[0] + 1
+                                    current_winner = "O"
+                                    with open("ASCII/P2.txt", encoding="utf-8") as f:
+                                        for i in f:
+                                            if line_num == 0:
+                                                pygame.mixer.music.pause()
+                                                music_stop[0] = not music_stop[0]
+                                                P_slam.play()
+                                            with term.location(50, line_num):
+                                                print(i, end="")
+                                                line_num = line_num + 1
+                                                if line_num == 7:
+                                                    time.sleep(2)
+                                                    Win_slam.play()
+                                        time.sleep(3)
+                                        pygame.mixer.music.unpause()
+                                        music_stop[0] = not music_stop[0]
+                            if len(memory_O) + len(memory_X) == 9 and line_num == 0:
                                 game_won = True
-                                x_won[0] = x_won[0] + 1
-                                current_winner = "X"
-                                with open("ASCII/P1.txt", encoding="utf-8") as f:
+                                with open("ASCII/TIE.txt", encoding="utf-8") as f:
+                                    pygame.mixer.music.pause()
+                                    music_stop[0] = not music_stop[0]
+                                    time.sleep(1)
+                                    Win_slam.play()
                                     for i in f:
-                                        if line_num == 0:
-                                            pygame.mixer.music.pause()
-                                            music_stop[0] = not music_stop[0]
-                                            P_slam.play()
                                         with term.location(50, line_num):
                                             print(i, end="")
                                             line_num = line_num + 1
-                                            if line_num == 7:
-                                                time.sleep(2)
-                                                Win_slam.play()
-                                    time.sleep(3)
-                                    pygame.mixer.music.unpause()
-                                    music_stop[0] = not music_stop[0]
-                            if all(sub in memory_O for sub in i) == True:
-                                game_won = True
-                                o_won[0] = o_won[0] + 1
-                                current_winner = "O"
-                                with open("ASCII/P2.txt", encoding="utf-8") as f:
-                                    for i in f:
-                                        if line_num == 0:
-                                            pygame.mixer.music.pause()
-                                            music_stop[0] = not music_stop[0]
-                                            P_slam.play()
-                                        with term.location(50, line_num):
-                                            print(i, end="")
-                                            line_num = line_num + 1
-                                            if line_num == 7:
-                                                time.sleep(2)
-                                                Win_slam.play()
-                                    time.sleep(3)
-                                    pygame.mixer.music.unpause()
-                                    music_stop[0] = not music_stop[0]
-                        if len(memory_O) + len(memory_X) == 9 and line_num == 0:
-                            game_won = True
-                            with open("ASCII/TIE.txt", encoding="utf-8") as f:
-                                pygame.mixer.music.pause()
+                                time.sleep(3)
+                                pygame.mixer.music.unpause()
                                 music_stop[0] = not music_stop[0]
-                                time.sleep(1)
-                                Win_slam.play()
-                                for i in f:
-                                    with term.location(50, line_num):
-                                        print(i, end="")
-                                        line_num = line_num + 1
-                            time.sleep(3)
-                            pygame.mixer.music.unpause()
-                            music_stop[0] = not music_stop[0]
 
-                        if game_won == True:
-                            with open("ASCII/REMATCH.txt", encoding="utf-8") as f:
-                                for i in f:
-                                    with term.location(
-                                        int(term.width * 0.75 - len(i) / 2), line_num2
-                                    ):
-                                        print(i, end="")
-                                        line_num2 = line_num2 + 1
+                            if game_won == True:
+                                with open("ASCII/REMATCH.txt", encoding="utf-8") as f:
+                                    for i in f:
+                                        with term.location(
+                                            int(term.width * 0.75 - len(i) / 2), line_num2
+                                        ):
+                                            print(i, end="")
+                                            line_num2 = line_num2 + 1
+
                 if game_won == True:
                     start_x = int(term.width * 0.75 - len(i) / 2)
-                    if (
-                        key.name
-                        and key.name.startswith("MOUSE_")
-                        and start_x <= key.mouse_xy[0] <= start_x + 18
-                        and 7 <= key.mouse_xy[1] <= 11
-                    ):
-                        with term.location(start_x, 11):
-                            print("‾" * 18, end="")
-                            if key.name == "MOUSE_LEFT":
-                                break
-                    else:
-                        with term.location(start_x, 11):
-                            print(" " * 18, end="")
+                    if key.name and key.name.startswith("MOUSE_"):
+                        if (
+                            start_x <= key.mouse_xy[0] <= start_x + 18
+                            and 7 <= key.mouse_xy[1] <= 11
+                        ):
+                            with term.location(start_x, 11):
+                                print("‾" * 18, end="")
+                                if key.name == "MOUSE_LEFT":
+                                    break
+                        else:
+                            with term.location(start_x, 11):
+                                print(" " * 18, end="")
 
-                    if (
-                        key.name
-                        and key.name.startswith("MOUSE_")
-                        and start_x + 79 <= key.mouse_xy[0] <= start_x + 92
-                        and 7 <= key.mouse_xy[1] <= 11
-                    ):
-                        with term.location(start_x + 79, 11):
-                            print("‾" * 13, end="")
-                            if key.name == "MOUSE_LEFT":
-                                newgame[0] = not newgame[0]
-                                break
-                    else:
-                        with term.location(start_x + 79, 11):
-                            print(" " * 13, end="")
+                        if (
+                            key.name
+                            and key.name.startswith("MOUSE_")
+                            and start_x + 79 <= key.mouse_xy[0] <= start_x + 92
+                            and 7 <= key.mouse_xy[1] <= 11
+                        ):
+                            with term.location(start_x + 79, 11):
+                                print("‾" * 13, end="")
+                                if key.name == "MOUSE_LEFT":
+                                    newgame[0] = not newgame[0]
+                                    break
+                        else:
+                            with term.location(start_x + 79, 11):
+                                print(" " * 13, end="")
 
     if x_won[0] > 99 or o_won[0] > 99:
         with term.fullscreen(), term.cbreak():
@@ -447,7 +448,7 @@ def game(x_won, o_won):
         return
 
     if current_winner == "X":
-        with term.fullscreen(), term.cbreak():
+        with term.fullscreen(), term.cbreak(), term.hidden_cursor():
             pygame.mixer.music.pause()
             music_stop[0] = not music_stop[0]
             with open("ASCII/P1.txt", encoding="utf-8") as f:
@@ -487,7 +488,7 @@ def game(x_won, o_won):
             time.sleep(2)
 
     if current_winner == "O":
-        with term.fullscreen(), term.cbreak():
+        with term.fullscreen(), term.cbreak(), term.hidden_cursor():
             pygame.mixer.music.pause()
             music_stop[0] = not music_stop[0]
             with open("ASCII/P1.txt", encoding="utf-8") as f:
@@ -527,7 +528,7 @@ def game(x_won, o_won):
             time.sleep(2)
 
     if current_winner == "":
-        with term.fullscreen(), term.cbreak():
+        with term.fullscreen(), term.cbreak(), term.hidden_cursor():
             pygame.mixer.music.pause()
             music_stop[0] = not music_stop[0]
             with open("ASCII/P1.txt", encoding="utf-8") as f:
