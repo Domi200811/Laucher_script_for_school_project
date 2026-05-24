d3dfd1a0835c1131327d975a0bf140831e0e1632
Domi200811 <pekdomi2008@gmail.com>
2026-05-25 01:32:45 +0200

    Made the first AI

--- story.py
+++ story.py
@@ -1,4 +1,5 @@
 from blessed import Terminal
+term= Terminal()
 import time
 import random
 import pygame
@@ -22,7 +23,6 @@ def typewriter(text, delay=0.04,):
         time.sleep(delay) """
 
 def transition(sleep=0.1):
-    term= Terminal()
     with term.hidden_cursor():
         temp=list(range(term.height))
         random.shuffle(temp)
@@ -30,9 +30,25 @@ def transition(sleep=0.1):
             with term.location(0, i):
                 print("█"*term.width, end="", flush=False)
                 time.sleep(sleep)
+def cell(x, y, color, term):
+    with term.location(x-2, y):
+        print(color + "═════" + term.normal)
+    with term.location(x-2, y+2):
+        print(color + "═════" + term.normal)
+    with term.location(x-3, y+1):
+        print(color + "║" + term.normal)
+    with term.location(x+3, y+1):
+        print(color + "║" + term.normal)
+    with term.location(x+3, y):
+        print(color + "╗" + term.normal)
+    with term.location(x-3, y):
+        print(color + "╔" + term.normal)
+    with term.location(x+3, y+2):
+        print(color + "╝" + term.normal)
+    with term.location(x-3, y+2):
+        print(color + "╚" + term.normal)
 
 def actIprologe():
-    term = Terminal()
     with term.fullscreen(), term.cbreak(), term.hidden_cursor():
         
         with term.location(0, 0):
@@ -53,45 +69,145 @@ def actIprologe():
         transition()
             
 def actI():
-    term = Terminal()
     tes = "X"
     x=int(term.width/2)
     y=3
     line_num = 0
-    WINNING_COMBOS = [
-        [[3, 2], [9, 2], [15, 2]],
-        [[3, 5], [9, 5], [15, 5]],
-        [[3, 8], [9, 8], [15, 8]],
-        [[3, 2], [3, 5], [3, 8]],
-        [[9, 2], [9, 5], [9, 8]],
-        [[15, 2], [15, 5], [15, 8]],
-        [[3, 2], [9, 5], [15, 8]],
-        [[15, 2], [9, 5], [3, 8]],
+    following = True
+    pos=[
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
     ]
-    board = f"""\
-     a     b     c
+    row=0
+    col=0
+    with term.fullscreen(), term.cbreak(), term.hidden_cursor():
+        while True:
+            
+            board = f"""\
+     a     b     c   
   ╔═════╦═════╦═════╗
-1 ║  {tes}  ║  {tes}  ║  {tes}  ║
+  ║  {pos[0][0]}  ║  {pos[0][1]}  ║  {pos[0][2]}  ║
   ╠═════╬═════╬═════╣
-2 ║     ║     ║     ║
+  ║  {pos[1][0]}  ║  {pos[1][1]}  ║  {pos[1][2]}  ║
   ╠═════╬═════╬═════╣
-3 ║     ║     ║     ║
+  ║  {pos[2][0]}  ║  {pos[2][1]}  ║  {pos[2][2]}  ║
   ╚═════╩═════╩═════╝
     """
-    board = io.StringIO(board)
-    
-    with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-        term = Terminal()
-        for i in board:
-            with term.location(int((term.width/2)-len(i)/2), line_num+2):
-                print(i, end="")
-                line_num= line_num+1
-        
-        while True:
-            with term.location(x-2, y):
-                print(term.blue+"═════"+term.normal)
+            
+            board = io.StringIO(board)
+            for i in board:
+                with term.location(int((term.width/2)-len(i)/2), line_num+2):
+                    print(i, end="")
+                    line_num= line_num+1
+            line_num=0
+            board.seek(0)
+            cell(x, y, term.red, term)
+            key = term.inkey()
+
+            with term.location(x, y+1):
+                print(" ", end="")
+
+            if key.name == "KEY_ESCAPE" or key == "q":
+                return
+            if key == "n":
+                pygame.mixer.music.stop()
+
+
+            if True:
+            
                 
-            term.inkey()
+                
+                if key == "w" or key.name == "KEY_UP":
+                    y = y - 2
+
+                if key == "s" or key.name == "KEY_DOWN":
+                    y = y + 2
+
+                if key == "d" or key.name == "KEY_RIGHT":
+                    x = x + 6
+
+                if key == "a" or key.name == "KEY_LEFT":
+                    x = x - 6
+
+                if x > int(term.width/2)+7:
+                    x = x - 6
+
+                if x < int(term.width/2)-7:
+                    x = x + 6
+
+                if y > 8:
+                    y = y - 2
+
+                if y < 3:
+                    y = y + 2
+                
+            if x==int((term.width/2)-6):
+                col=0
+            if x==int((term.width/2)):
+                col=1
+            if x==int((term.width/2)+6):
+                col=2
+            if y==3:
+                row=0
+            if y==5:
+                row=1
+            if y==7:
+                row=2
+            with term.location(0,0):
+                print(col, row)
+            with term.location(0,1):
+                print(x, y)
+                
+                if key.name == "KEY_ENTER" or key==" ":
+                    if following and pos[row][col]==" ":
+                        pos[row][col]="X"
+                        following = not following
+                    while not following:
+                        temp=0
+                        for i in pos:
+                            for a in i:
+                                if a!=" ":
+                                    temp+=1
+                        if temp!=9:
+                            rand_1=random.randint(0, 2)
+                            rand_2=random.randint(0, 2)
+                            if pos[rand_1][rand_2]==" ":
+                                pos[rand_1][rand_2]="O"
+                                following = not following
+                        else:
+                            break
+                            
+                            
+                    for i in range(0, 2):
+                        if pos[i][0]==pos[i][1]==pos[i][2]=="X":
+                            with term.location(0,3):
+                                print("X won")
+                        if pos[i][0]==pos[i][1]==pos[i][2]=="O":
+                            with term.location(0,3):
+                                print("O won")
+                        if pos[0][i]==pos[1][i]==pos[2][i]=="X":
+                            with term.location(0,3):
+                                print("X won")
+                        if pos[0][i]==pos[1][i]==pos[2][i]=="O":
+                            with term.location(0,3):
+                                print("O won")
+                        if pos[0][0]==pos[1][1]==pos[2][2]=="X":
+                            with term.location(0,3):
+                                print("X won")
+                        if pos[2][0]==pos[1][1]==pos[0][2]=="X":
+                            with term.location(0,3):
+                                print("X won")
+                        if pos[0][0]==pos[1][1]==pos[2][2]=="O":
+                            with term.location(0,3):
+                                print("O won")
+                        if pos[2][0]==pos[1][1]==pos[0][2]=="O":
+                            with term.location(0,3):
+                                print("O won")
+                    if temp==9:
+                        with term.location(0,3):
+                            print("draw")
+
 
 
 actI()
